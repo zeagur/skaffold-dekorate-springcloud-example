e5c1ed55aa9dc104ad44a19189550463c64f2c7c
zeagur <43737901+zeagur@users.noreply.github.com>
2020-07-10 22:38:37 +0700

    Changes: - Update Annotation to use Springbootapplication instead due Dekorate unable to issue with Springcloudapplication annotation - Correct Labels configuration - Add sample custom output path

--- src/main/java/sample/proj/Application.java
+++ src/main/java/sample/proj/Application.java
@@ -1,17 +1,14 @@
 package sample.proj;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @HystrixCommand
-    public String simpleCommand() {
-        return "Do some fallback procedure here";
-    }
 }
